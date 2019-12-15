0498dcac5283db858edf9c054cd0c9e0b98622f5
Jaymeet Mehta <17it049@charusat.edu.in>
2019-12-15 19:50:03 +0530

    Practical 1 Done

---
+++ practical_1/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  Container(
+    color: Colors.yellow,
+    child: Center(
+      child:Text("Welcome to Practical 1",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 32.0),
+    ),
+  ))
+);
